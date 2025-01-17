3caac8bd06a7cb8ffd59c1613acf130164c790de
X-spec7 <mercury.spec77@gmail.com>
2025-01-18 06:37:07 +0900

    fix: fix contact user serializer

--- backend/chat/serializers.py
+++ backend/chat/serializers.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from rest_framework import serializers
 from backend.chat.models import Contact, Message
 
@@ -10,11 +11,18 @@ class MessageSerializer(serializers.ModelSerializer):
         fields = ["id", "content", "isRead", "sentDate"]
 
 class ContactUserSerializer(serializers.ModelSerializer):
+    avatarUrl = serializers.SerializerMethodField()
+
     fullName = serializers.CharField(source="contact.full_name")
-    avatarUrl = serializers.ImageField(source="contact.avatar_image")
     userType = serializers.CharField(source="contact.user_type")
     lastMessage = MessageSerializer(source="last_message")
 
     class Meta:
         model = Contact
         fields = ["id", "fullName", "avatarUrl", "userType", "unreadCount", "lastMessage"]
+
+    def get_avatarUrl(self, obj):
+        if obj.contact.avatar_image:
+            return self.context["request"].build_absolute_uri(obj.contact.avatar_image.url)
+        return None
+
