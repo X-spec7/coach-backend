84707a7c79c4e4d86cbecda8a2dd634455b3c804
X-spec7 <mercury.spec77@gmail.com>
2025-01-29 06:52:13 +0900

    fix: api router settings in authentication and users

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from .views import GetUserProfileView, UpdateUserProfileView
 
 app_name = "users"
-urlpatterns = []
+urlpatterns = [
+  path("profile/get/", view=GetUserProfileView.as_view(), name="get_user_profile"),
+  path("profile/update/", view=UpdateUserProfileView.as_view(), name="update_user_profile")
+]
