48fbaaa66d4a58446b8578fee0a97df99e03f03f
X-spec7 <mercury.spec77@gmail.com>
2025-01-29 06:49:25 +0900

    refactor: moved get and update user profile view from authentication to users api router

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,44 +1,78 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.messages.views import SuccessMessageMixin
+import os
+import requests
 from django.urls import reverse
+from rest_framework.views import APIView
+from rest_framework import status, permissions
+from rest_framework.permissions import AllowAny, IsAuthenticated
+import base64
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.response import Response
 from django.utils.translation import gettext_lazy as _
-from django.views.generic import DetailView
-from django.views.generic import RedirectView
-from django.views.generic import UpdateView
 
 from backend.users.models import User
+from .serializers import UserSerializer
+from django.conf import settings
 
+class GetUserProfileView(APIView):
+  permission_classes = [IsAuthenticated]
+  authentication_classes = [JWTAuthentication]
 
-class UserDetailView(LoginRequiredMixin, DetailView):
-    model = User
-    slug_field = "id"
-    slug_url_kwarg = "id"
+  def get(self, request):
+      user = request.user
 
+      return Response(
+          {
+              "message": "Successfully fetched user profile",
+              "user": UserSerializer(user).data,
+          },
+          status=status.HTTP_200_OK,
+      )
+  
+class UpdateUserProfileView(APIView):
+  permission_classes = [IsAuthenticated]
+  authentication_classes = [JWTAuthentication]
 
-user_detail_view = UserDetailView.as_view()
+  def post(self, request):
+    try:
+        data = request.data
+        user = request.user
 
+        # Update user profile fields if they are not blank
+        if 'phone_number' in data and data['phoneNumebr']:
+            user.phone_number = data['phoneNumber']
+        if 'address' in data and data['address']:
+            user.address = data['address']
+        if 'first_name' in data and data['firstName']:
+            user.first_name = data['firstName']
+        if 'last_name' in data and data['lastName']:
+            user.last_name = data['lastName']
 
-class UserUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
-    model = User
-    fields = ["name"]
-    success_message = _("Information successfully updated")
+        avatar_image_base64 = data.get('avatarImage')
+        if avatar_image_base64:
+            # Decode the Base64 string
+            format, imgstr = avatar_image_base64.split(';base64,')  # Split format and data
+            ext = format.split('/')[-1]  # Extract the image file extension (e.g., jpg, png)
+            file_name = f"{user.id}_avatar.{ext}"
+            file_path = os.path.join(settings.MEDIA_ROOT, 'avatar_images', file_name)
+            os.makedirs(os.path.dirname(file_path), exist_ok=True)  # Ensure the directory exists
 
-    def get_success_url(self):
-        # for mypy to know that the user is authenticated
-        assert self.request.user.is_authenticated
-        return self.request.user.get_absolute_url()
+            # Save the file
+            with open(file_path, "wb") as f:
+                f.write(base64.b64decode(imgstr))
 
-    def get_object(self):
-        return self.request.user
+            # Save the relative path to the user's profile
+            user.avatar_image = f"avatar_images/{file_name}"
 
-user_update_view = UserUpdateView.as_view()
+        user.save()
 
+        userSerializer = UserSerializer(user)
+        return Response(
+            {
+                "message": "Profile Updated successfully.",
+                "user": userSerializer.data,
+            },
+            status=status.HTTP_200_OK
+        )
 
-class UserRedirectView(LoginRequiredMixin, RedirectView):
-    permanent = False
-
-    def get_redirect_url(self):
-        return reverse("users:detail", kwargs={"pk": self.request.user.pk})
-
-
-user_redirect_view = UserRedirectView.as_view()
+    except Exception as e:
+        return Response({"error": str(e)}, status=400)
