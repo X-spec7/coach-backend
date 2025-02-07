b689a5d58300a66d28b165a7505a64c08951f6d8
X-spec7 <mercury.spec77@gmail.com>
2025-02-07 11:39:22 +0900

    feat: create custom permission

--- backend/permissions.py
+++ backend/permissions.py
@@ -8,3 +8,18 @@ class IsAdminUserOnly(BasePermission):
   def has_permission(self, request, view):
     return request.user and request.user.is_staff
   
+class IsCoachUserOnly(BasePermission):
+  """
+  Custom permission to allow only coach users to access the view.
+  """
+
+  def has_permission(self, request, view):
+    return request.user and request.user.user_type == "Coach"
+  
+class IsClientUserOnly(BasePermission):
+  """
+  Custom permission to allow only client users to access the view.
+  """
+
+  def has_permission(self, request, view):
+    return request.user and request.user.user_type == "Client"
